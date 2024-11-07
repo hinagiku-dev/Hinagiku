2b69a4c70a2a9168ba85ce839781a87b0212a607
JacobLinCool <jacoblincool@gmail.com>
2024-11-08 00:09:30 +0800

    feat: support service account object

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,11 +1,22 @@
 import { env } from '$env/dynamic/private';
-import { cert, getApps, initializeApp } from 'firebase-admin/app';
+import { cert, getApps, initializeApp, type ServiceAccount } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
 if (!getApps().length) {
+	let serviceAccountPathOrObject: string | ServiceAccount;
+	try {
+		const tmp = JSON.parse(env.GOOGLE_APPLICATION_CREDENTIALS);
+		if (typeof tmp === 'object') {
+			serviceAccountPathOrObject = tmp;
+		} else {
+			throw new Error();
+		}
+	} catch {
+		serviceAccountPathOrObject = env.GOOGLE_APPLICATION_CREDENTIALS;
+	}
 	initializeApp({
-		credential: cert(env.GOOGLE_APPLICATION_CREDENTIALS)
+		credential: cert(serviceAccountPathOrObject)
 	});
 }
 
