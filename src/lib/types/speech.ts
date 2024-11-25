dca332d56e9487399f19f1ffb6be3ba019ebb2bf
HowChienLu <53228984+howard9199@users.noreply.github.com>
2024-11-26 03:32:32 +0800

    fix: some typos, complete the speech.ts
    但我不知道 speechId 和 fileId 差在哪裡

--- src/lib/types/speech.ts
+++ src/lib/types/speech.ts
@@ -1,5 +1,19 @@
-export interface SpeechFirestore {
+export interface FirestoreSpeech {
 	speechId: string;
-	fileid: string;
-	transation: string;
+	fileId: string;
+	transcription: string;
+}
+
+export interface Speech {
+	speechId: string;
+	fileId: string;
+	transcription: string;
+}
+
+export function convertFirestoreSpeech(data: FirestoreSpeech): Speech {
+	return {
+		speechId: data.speechId,
+		fileId: data.fileId,
+		transcription: data.transcription
+	};
 }
