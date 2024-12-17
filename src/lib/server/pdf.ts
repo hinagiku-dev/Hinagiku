dfa896a0ebdac74697b1ee50cab2b37d6f85a346
Takala <dylan91011028@gmail.com>
2024-12-17 23:16:34 +0800

    fix: support script for pdf2text

--- src/lib/server/pdf.ts
+++ src/lib/server/pdf.ts
@@ -1,20 +1,14 @@
 // import pdf from 'pdf-parse';
-import { env } from '$env/dynamic/private';
 import { LlamaParseReader } from 'llamaindex';
 
-export async function pdf2Text(fileBuffer: ArrayBuffer): Promise<string | null> {
+export async function pdf2Text(fileBuffer: ArrayBuffer, apiKey: string): Promise<string | null> {
 	try {
 		const reader = new LlamaParseReader({
 			resultType: 'markdown',
-			apiKey: env.LLAMA_CLOUD_API_KEY
+			apiKey: apiKey
 		});
 		const documents = await reader.loadDataAsContent(new Uint8Array(fileBuffer));
 		return documents.map((doc) => doc.text).join('\n');
-
-		// const data = await pdf(Buffer.from(fileBuffer));
-		// const text = data.text;
-		// data.text = text.replace(/^\s*\n+/gm, '\n');
-		// return data.text;
 	} catch (error) {
 		console.error('Error in pdf2Text', error);
 		return null;
