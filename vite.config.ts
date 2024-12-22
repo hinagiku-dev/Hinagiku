13d8bd6c2267091404ca607106a70425a7cb13cb
JacobLinCool <jacoblincool@gmail.com>
2024-12-22 23:30:10 +0800

    build: no bundle wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: 'node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js',
-					dest: './'
-				},
-				{
-					src: 'node_modules/@ricky0123/vad-web/dist/silero_vad_v5.onnx',
-					dest: './'
-				},
-				{
-					src: 'node_modules/onnxruntime-web/dist/*.wasm',
-					dest: './'
-				},
-				{
-					src: 'node_modules/onnxruntime-web/dist/ort-wasm-simd-threaded.mjs',
-					dest: './'
-				}
-			]
-		})
-	]
+	plugins: [sveltekit()]
 });
