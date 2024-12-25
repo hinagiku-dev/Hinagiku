5959c9ad985faf187e1f2cb1d496fcea90c63be0
林宏昀 <112567202+Buffett111@users.noreply.github.com>
2024-12-25 20:15:30 +0800

    fix: wrong profile input (#94)

--- src/routes/api/auth/signin/+server.ts
+++ src/routes/api/auth/signin/+server.ts
@@ -33,6 +33,8 @@ export const POST: RequestHandler = async ({ request, cookies }) => {
 				uid,
 				displayName: user.displayName || 'User',
 				email: user.email || '',
+				title: '',
+				bio: '',
 				createdAt: FieldValue.serverTimestamp(),
 				updatedAt: FieldValue.serverTimestamp()
 			};
