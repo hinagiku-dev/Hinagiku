7d3c2cbfe8045dcfd5c1bae7209ee84227aa5a50
JacobLinCool <jacoblincool@gmail.com>
2024-12-27 05:10:22 +0800

    refactor: use session card in sessions page

--- src/routes/dashboard/recent/host/+page.svelte
+++ src/routes/dashboard/recent/host/+page.svelte
@@ -2,13 +2,12 @@
 	import { onDestroy } from 'svelte';
 	import { Card, Button } from 'flowbite-svelte';
 	import { MessageSquarePlus } from 'lucide-svelte';
-	import { collection, orderBy, query, where, Timestamp } from 'firebase/firestore';
-	import { user } from '$lib/stores/auth';
-	import { profile } from '$lib/stores/profile';
+	import { collection, orderBy, query, where, Timestamp, limit } from 'firebase/firestore';
 	import { db } from '$lib/firebase';
 	import { subscribeAll } from '$lib/firebase/store';
 	import type { Session } from '$lib/schema/session';
 	import { writable, derived } from 'svelte/store';
+	import SessionCard from '$lib/components/SessionCard.svelte';
 
 	let { data } = $props();
 
@@ -16,7 +15,8 @@
 		query(
 			collection(db, 'sessions'),
 			where('host', '==', data.user.uid),
-			orderBy('createdAt', 'desc')
+			orderBy('createdAt', 'desc'),
+			limit(100)
 		)
 	);
 
@@ -62,19 +62,14 @@
 <main class="mx-auto max-w-6xl px-4 py-16">
 	<div class="mb-12 flex items-center justify-between">
 		<div>
-			<h1 class="text-3xl font-bold text-gray-900">Recent Sessions</h1>
-			<p class="mt-2 text-gray-600">Welcome back, {$profile?.displayName || $user?.displayName}</p>
+			<h1 class="text-3xl font-bold text-gray-900">Recent Hosted Sessions</h1>
 		</div>
 		<div class="text-right">
 			<Button href="/dashboard">Back to Dashboard</Button>
 		</div>
 	</div>
 
-	<!-- Recent Session -->
-	<div class="mt-16">
-		<div class="mb-6 flex items-center justify-between">
-			<h2 class="text-2xl font-semibold text-gray-900">Recent Host Sessions</h2>
-		</div>
+	<div class="my-8">
 		<div class="mb-4 flex flex-wrap gap-2">
 			{#each $availableLabels as label}
 				<Button
@@ -89,42 +84,14 @@
 		<div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 			{#if $filteredHostSessions?.length}
 				{#each $filteredHostSessions as [doc, session]}
-					<Card padding="lg" class="transition-all hover:border-primary-500">
-						<div>
-							<div class="mb-4 flex items-start justify-between">
-								<h3 class="line-clamp-1 text-xl font-bold">{session.title}</h3>
-								<span
-									class="rounded-full px-3 py-1 text-sm font-medium {session.status === 'preparing'
-										? 'bg-yellow-100 text-yellow-600'
-										: session.status === 'individual'
-											? 'bg-blue-100 text-blue-600'
-											: session.status === 'before-group'
-												? 'bg-purple-100 text-purple-600'
-												: session.status === 'group'
-													? 'bg-green-100 text-green-600'
-													: 'bg-gray-100 text-gray-600'}"
-								>
-									{session.status}
-								</span>
-							</div>
-							<div class="mb-4 flex min-h-[28px] flex-wrap gap-2">
-								{#if session.labels?.length}
-									{#each session.labels.sort() as label}
-										<span class="rounded-full bg-gray-100 px-2 py-1 text-xs text-gray-600">
-											{label}
-										</span>
-									{/each}
-								{/if}
-							</div>
-							<p class="mb-4 line-clamp-2 text-gray-600">{session.task}</p>
-							<div class="mb-4 flex items-center gap-4">
-								<span class="text-sm text-gray-500">
-									{(session.createdAt as Timestamp).toDate().toLocaleString()}
-								</span>
-							</div>
-							<Button href="/session/{doc.id}" class="w-full">View Session</Button>
-						</div>
-					</Card>
+					<SessionCard
+						id={doc.id}
+						title={session.title}
+						status={session.status}
+						labels={session.labels}
+						task={session.task}
+						createdAt={(session.createdAt as Timestamp).toDate()}
+					/>
 				{/each}
 			{:else}
 				<Card class="md:col-span-2 lg:col-span-3">
