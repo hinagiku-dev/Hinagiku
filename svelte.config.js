3e67e02a127f311f5cdafe221f591b0f8464a58e
JacobLinCool <jacoblincool@gmail.com>
2025-02-20 11:52:35 +0800

    Dockerize and CI (#145)
    * build: dockerize
    
    * ci: build and push image to ghcr

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+
+const adapter = process.env.SVELTEKIT_ADAPTER === 'node' ? adapterNode : adapterAuto;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +12,11 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		// adapter-auto only supports some environments, see
+		// https://svelte.dev/docs/kit/adapter-auto for a list. If your environment
+		// is not supported, or you settled on a specific environment, switch out
+		// the adapter. See https://svelte.dev/docs/kit/adapters for more
+		// information about adapters.
 		adapter: adapter()
 	},
 
