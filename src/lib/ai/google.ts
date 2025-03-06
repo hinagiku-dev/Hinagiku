09db21affbfa1a70d859eb889cc431dd103aeed3
JacobLinCool <jacoblincool@gmail.com>
2025-03-06 15:19:56 +0800

    feat: use gemini 2.0 flash

--- src/lib/ai/google.ts
+++ src/lib/ai/google.ts
@@ -1,8 +1,8 @@
 import { env } from '$env/dynamic/private';
-import { gemini20FlashExp, googleAI } from '@genkit-ai/googleai';
+import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
 import { genkit } from 'genkit';
 
 export const GoogleGeminiFlash = genkit({
 	plugins: [googleAI({ apiKey: env.GOOGLE_GENAI_API_KEY })],
-	model: gemini20FlashExp
+	model: gemini20Flash
 });
