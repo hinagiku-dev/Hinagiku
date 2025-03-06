283cdd8044ce8644f223d4a60b69353550948ccb
JacobLinCool <jacoblincool@gmail.com>
2025-03-05 23:27:29 -0800

    Pdf parser with gemini (#154)
    * feat: enhance prompt structure with XML-like tags for clarity
    
    * chore: update packageManager to pnpm@10.5.1
    
    * feat: update pdf2Text function to use GoogleGeminiFlash for PDF parsing
    
    * feat: use gemini 2.0 flash
    
    * chore: upgrade deps
    
    * chore: add @eslint/js
    
    ---------
    
    Co-authored-by: Takala <dylan91011028@gmail.com>

--- src/lib/server/pdf.ts
+++ src/lib/server/pdf.ts
@@ -1,11 +1,42 @@
 // import pdf from 'pdf-parse';
-import { LlamaParse } from './llamaparse';
+import { GoogleGeminiFlash, z } from '$lib/ai';
+import { PDF_PARSE_PROMPT } from './prompt';
 
-export async function pdf2Text(fileBuffer: ArrayBuffer, apiKey: string): Promise<string | null> {
+// export async function pdf2Text(fileBuffer: ArrayBuffer, apiKey: string): Promise<string | null> {
+// 	try {
+// 		const parser = new LlamaParse(apiKey);
+// 		const result = await parser.parseFile(new Blob([fileBuffer]));
+// 		return result.markdown;
+// 	} catch (error) {
+// 		console.error('Error in pdf2Text', error);
+// 		return null;
+// 	}
+// }
+
+export async function pdf2Text(fileBuffer: ArrayBuffer): Promise<string | null> {
 	try {
-		const parser = new LlamaParse(apiKey);
-		const result = await parser.parseFile(new Blob([fileBuffer]));
-		return result.markdown;
+		const { output } = await GoogleGeminiFlash.generate({
+			system: PDF_PARSE_PROMPT,
+			prompt: [
+				{
+					media: {
+						url: `data:application/pdf;base64,${Buffer.from(fileBuffer).toString('base64')}`
+					}
+				}
+			],
+			output: {
+				schema: z.object({
+					markdown: z.string()
+				})
+			},
+			config: { temperature: 0.1 }
+		});
+
+		if (!output) {
+			throw new Error('Failed to parse PDF');
+		}
+
+		return output.markdown;
 	} catch (error) {
 		console.error('Error in pdf2Text', error);
 		return null;
