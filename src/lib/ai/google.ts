0543ca517c9d1edf30d0ed7d2cc67b981060d66f
Takala <dylan91011028@gmail.com>
2025-05-05 19:30:48 +0800

    feat: update transcription to use asrModel for audio processing

--- src/lib/ai/google.ts
+++ src/lib/ai/google.ts
@@ -1,8 +1,13 @@
 import { env } from '$env/dynamic/private';
 import { genkit } from 'genkit';
-import { gpt41Mini, openAI } from 'genkitx-openai';
+import { gpt41Mini, gpt4oTranscribe, openAI } from 'genkitx-openai';
 
 export const llmModel = genkit({
 	plugins: [openAI({ apiKey: env.OPENAI_API_KEY })],
 	model: gpt41Mini
+});
+
+export const asrModel = genkit({
+	plugins: [openAI({ apiKey: env.OPENAI_API_KEY })],
+	model: gpt4oTranscribe
 });
