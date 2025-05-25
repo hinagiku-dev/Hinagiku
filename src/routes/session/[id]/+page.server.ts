ab8a78b3b9457dc00f95c2e94ade41a16f8db75f
180079995 <101566121+180079995@users.noreply.github.com>
2025-05-25 23:30:14 +0800

    qrcode redirect fix

--- src/routes/session/[id]/+page.server.ts
+++ src/routes/session/[id]/+page.server.ts
@@ -3,13 +3,33 @@ import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, locals, url }) => {
-	if (!locals.user) {
-		// TODO: classCode
-		throw redirect(303, '/login?then=' + encodeURIComponent(url.pathname) + '&classCode=test');
-	}
-
 	const sessionRef = adminDb.collection('sessions').doc(params.id);
 	const sessionDoc = await sessionRef.get();
+
+	if (!locals.user) {
+		const classId = sessionDoc.data()?.classId;
+		if (!classId) {
+			throw redirect(303, '/login?then=' + encodeURIComponent(url.pathname));
+		}
+		let classCode = '';
+		const classRef = adminDb.collection('classes').doc(classId);
+		const classDoc = await classRef.get();
+		if (!classDoc.exists) {
+			throw error(404, 'Class not found');
+		}
+		if (classDoc.data()?.code) {
+			classCode = classDoc.data()?.code;
+		} else {
+			throw error(404, 'Class code not found');
+		}
+		if (!classCode || classCode === '') {
+			throw redirect(303, '/login?then=' + encodeURIComponent(url.pathname));
+		}
+		throw redirect(
+			303,
+			'/login?then=' + encodeURIComponent(url.pathname) + '&classCode=' + classCode
+		);
+	}
 
 	if (!sessionDoc.exists) {
 		throw error(404, 'Session not found');
