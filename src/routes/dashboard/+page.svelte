9b2a32678c7eb9b3d99f143fe6be08d7da24163a
Andy Lu (Lu, Yu-An) <naoyt4146@gmail.com>
2025-05-26 00:01:48 +0800

    feat: show className at Sessions hosted by user in dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -313,6 +313,7 @@
 						labels={session.labels}
 						task={session.task}
 						createdAt={(session.createdAt as Timestamp).toDate()}
+						classId={session.classId}
 					/>
 				{/each}
 			{:else}
